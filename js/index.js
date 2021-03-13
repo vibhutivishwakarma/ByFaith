670959fc841592a541db79333962708e37d3e5d1
vibhutivishwakarma <77892181+vibhutivishwakarma@users.noreply.github.com>
2021-03-13 08:39:19 +0530

    Commit Full Code

---
+++ js/index.js
@@ -0,0 +1,61 @@
+// $(document).ready(function(){
+         
+//     $('.testimonial').owlCarousel({
+//       loop:true,
+//       margin:10,
+//       nav:true,
+//       responsive:{
+//           0:{
+//               items:1
+//           },
+//           600:{
+//               items:3
+//           },
+//           1000:{
+//               items:5
+//           }
+//       }
+//   })
+// });
+
+
+
+$(document).ready(function(){
+    
+    $('.testimonial').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        prevArrow: '<i class="fa fa-long-arrow-left left-arrow" aria-hidden="true"></i>',
+        nextArrow: '<i class="fa fa-long-arrow-right right-arrow" aria-hidden="true"></i>',
+        // slide: 'div',  
+        // nextArrow: '<button class="slick-next">Next >',
+        //     prevArrow: '<button class="slick-prev">previous >',  
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+
+});
+
+
